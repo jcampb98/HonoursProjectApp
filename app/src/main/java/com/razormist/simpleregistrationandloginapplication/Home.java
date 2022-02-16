fd0b3895693e9f2c2240cb4c88947767a86c79e7
jcampb98 <caboose768@gmail.com>
2022-02-16 14:21:48 +0000

    created and implemented the screen broadcast receiver into the home page class
    Signed-off-by: jcampb98 <caboose768@gmail.com>

--- app/src/main/java/com/razormist/simpleregistrationandloginapplication/Home.java
+++ app/src/main/java/com/razormist/simpleregistrationandloginapplication/Home.java
@@ -1,12 +1,15 @@
 package com.razormist.simpleregistrationandloginapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.solver.ArrayLinkedVariables;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +19,10 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
 
+    }
+    
+    public void bootReceiver(Context context, Intent intent) {
+        Intent i = new Intent(context, ScreenTimeBroadcastReceiver.class);
+        context.startService(i);
     }
 }
