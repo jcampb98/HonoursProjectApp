4ecbbe19cabc24fa617fc8535d08fea4309c6872
jcampb98 <caboose768@gmail.com>
2022-02-22 10:05:41 +0000

    created and implemented the logout button function into the home page class
    Signed-off-by: jcampb98 <caboose768@gmail.com>

--- app/src/main/java/com/razormist/simpleregistrationandloginapplication/Home.java
+++ app/src/main/java/com/razormist/simpleregistrationandloginapplication/Home.java
@@ -1,26 +1,60 @@
 package com.razormist.simpleregistrationandloginapplication;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.constraint.solver.ArrayLinkedVariables;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
+    Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logout = (Button) findViewById(R.id.buttonLogout);
     }
 
-    @Override
-    public void onClick(View view) {
-
+    public void onClick(View v) {
+        //Checks for exit button
+        if(v == logout){
+            showtbDialog();
+        }
     }
-    
+
+    private void showtbDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to Logout??");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id)
+            {
+                Toast.makeText(getApplicationContext(), "You Pressed Yes", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id)
+            {
+                Toast.makeText(getApplicationContext(), "You Pressed No", Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
     public void bootReceiver(Context context, Intent intent) {
         Intent i = new Intent(context, ScreenTimeBroadcastReceiver.class);
         context.startService(i);
