e32190bea326d7b226fdfde29724a96e0c881413
jcampb98 <caboose768@gmail.com>
2022-02-22 12:28:34 +0000

    implemented the UsageStatsManager feature into the application
    Signed-off-by: jcampb98 <caboose768@gmail.com>

--- app/src/main/java/com/razormist/simpleregistrationandloginapplication/Home.java
+++ app/src/main/java/com/razormist/simpleregistrationandloginapplication/Home.java
@@ -1,20 +1,39 @@
 package com.razormist.simpleregistrationandloginapplication;
 
+import android.app.AppOpsManager;
+import android.app.usage.UsageStats;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.constraint.solver.ArrayLinkedVariables;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.app.usage.UsageStatsManager;
 
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.os.Process.myUid;
+
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class Home extends AppCompatActivity implements View.OnClickListener {
     Button logout;
+    TextView permissionMessage;
+    private final Context context;
+
+    private UsageStatsManager usageStatsManager;
+
+    public Home(Context context) {
+        this.context = context;
+        usageStatsManager = (UsageStatsManager)
+                context.getSystemService(Context.USAGE_STATS_SERVICE);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +41,9 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_home);
 
         logout = (Button) findViewById(R.id.buttonLogout);
+        permissionMessage = (TextView) findViewById(R.id.grant_permission_message);
+
+        permissionMessage.setOnClickListener(v -> openSettings());
     }
 
     public void onClick(View v) {
@@ -53,5 +75,16 @@ public class Home extends AppCompatActivity implements View.OnClickListener {
         });
         AlertDialog alert = builder.create();
         alert.show();
+    }
+
+    private void openSettings() {
+        startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+    }
+
+    private boolean checkForPermission(Context context) {
+        AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, myUid(), context.getPackageName());
+
+        return mode == MODE_ALLOWED;
     }
 }
